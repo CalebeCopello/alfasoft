64327c6e6323d37f40660e8c125a7dcf5c7cc680
Calebe Copello <calebecopello@gmail.com>
2025-04-08 14:03:59 -0300

    added show blade view to view contact information

---
+++ html/resources/views/contacts/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Contact Details</h1>
+
+    <div class="card mb-3">
+        <div class="card-body">
+            <h4 class="card-title">{{$contact->name}}</h4>
+            <p class="card-text"><strong>ID: </strong>{{$contact->id}}</p>
+            <p class="card-text"><strong>Contact Number: </strong>{{$contact->contact}}</p>
+            <p class="card-text"><strong>Email: </strong>{{$contact->email}}</p>
+            <p class="card-text"><strong>Created At: </strong>{{$contact->created_at->toDayDateTimeString()}}</p>
+        </div>
+    </div>
+    <a href="{{ route('contacts.edit', $contact->id) }}" class="btn btn-warning">Edit</a>
+</div>
+@endsection
