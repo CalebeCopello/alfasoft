308be819a578a9cc0ad911a4f4bddb8d3acb2fb7
Calebe Copello <calebecopello@gmail.com>
2025-04-08 12:47:04 -0300

    added create and store functions

--- html/app/Http/Controllers/ContactController.php
+++ html/app/Http/Controllers/ContactController.php
@@ -11,4 +11,17 @@ class ContactController extends Controller
         $contacts = Contact::all();
         return view('contacts.index', compact('contacts'));
     }
+    public function create() {
+        return view('contacts.create');
+    }
+
+    public function store(Request $request) {
+        $validated = $request->validate([
+            'name' => 'required|min:5',
+            'contact' => 'required|unique:contacts,contact|digits:9',
+            'email' => 'required|email|unique:contacts,email'
+        ]);
+        $contact = Contact::create($validated);
+        return redirect()->route('contacts.index')->with('success', "Contact <strong> {$contact->name} </strong> created successfully.");
+    }
 }
