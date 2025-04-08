adb447c3dbd9571719b42aaa5a5777034e46b8d3
Calebe Copello <calebecopello@gmail.com>
2025-04-08 14:05:56 -0300

    added index blade view to list all contacts information

---
+++ html/resources/views/contacts/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    @if (session('success'))
+        <div class="alert alert-success">
+            {!! session('success') !!}
+        </div>
+    @endif
+    <h1 class="mb-4">Contact List</h1>
+
+        <a href="{{ route('contacts.create') }}" class="btn btn-primary mb-3">Add New Contact</a>
+
+    @if ($contacts->isEmpty())
+        <p>No contacts found.</p>
+    @else
+        <table class="table table-bordered">
+            <thead class="thead-light">
+                <tr>
+                    <th>Name</th>
+                    <th>Contact</th>
+                    <th>Email</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($contacts as $contact)
+                    <tr>
+                        <td>{{ $contact->name }}</td>
+                        <td>{{ $contact->contact }}</td>
+                        <td>{{ $contact->email }}</td>
+                        <td>
+                            <a href="{{ route('contacts.show', $contact) }}" class="btn btn-sm btn-info">View</a>
+                            <a href="{{ route('contacts.edit', $contact) }}" class="btn btn-sm btn-warning">Edit</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+</div>
+@endsection
